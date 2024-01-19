a90407c0cff14817e31a23ab2f476b82f201e171
Rémi Cérès <remi.ceres@inria.fr>
2024-01-19 21:18:40 +0100

    Update .eslintrc.js configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,49 +1,40 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es2021': true
+    env: {
+        browser: true,
+        es2021: true,
     },
-    'extends': [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended'
-    ],
-    'overrides': [
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+    overrides: [
         {
-            'env': {
-                'node': true
+            env: {
+                node: true,
             },
-            'files': [
-                '.eslintrc.{js,cjs}'
-            ],
-            'parserOptions': {
-                'sourceType': 'script'
-            }
-        }
+            files: ['.eslintrc.{js,cjs}'],
+            parserOptions: {
+                sourceType: 'script',
+            },
+        },
     ],
-    'parser': '@typescript-eslint/parser',
-    'parserOptions': {
-        'ecmaVersion': 'latest',
-        'sourceType': 'module'
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
     },
-    'plugins': [
-        '@typescript-eslint'
-    ],
-    'rules': {
-        'indent': [
+    plugins: ['@typescript-eslint'],
+    rules: {
+        indent: ['error', 4, { 'SwitchCase': 1 }], 
+        'linebreak-style': ['error', 'unix'],
+        quotes: ['error', 'single'],
+        semi: ['error', 'always'],
+        '@typescript-eslint/naming-convention': [
             'error',
-            4
+            {
+                selector: 'memberLike',
+                modifiers: ['private'],
+                format: ['camelCase'],
+                leadingUnderscore: 'require',
+            },
         ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'always'
-        ]
-    }
+        'max-len': ['error', 120],
+    },
 };
