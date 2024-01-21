fbe9671a33f486c2b5781e46f482c06f880b032c
Rémi Cérès <remi.ceres@inria.fr>
2024-01-21 20:53:48 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,19 @@
-import { realpathSync } from 'fs';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import { resolve as _resolve } from 'path';
-import process from 'process';
+const { realpathSync } = require('fs');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { resolve: _resolve } = require('path');
+const process = require('process');
 
 const appDirectory = realpathSync(process.cwd());
 
-export const entry = _resolve(appDirectory, 'src/App.ts');
-export const output = {
+const entry = _resolve(appDirectory, 'src/App.ts');
+const output = {
     filename: 'js/bundleName.js', //name for the js file that is created/compiled in memory
     clean: true,
 };
-export const resolve = {
+const resolve = {
     extensions: ['.tsx', '.ts', '.js'],
 };
-export const devServer = {
+const devServer = {
     host: '0.0.0.0',
     port: 8080, //port that we're using for local host (localhost:8080)
     static: _resolve(appDirectory, 'public'), //tells webpack to serve from the public folder
@@ -22,7 +22,7 @@ export const devServer = {
         publicPath: '/',
     },
 };
-export const module = {
+const moduleRules = {
     rules: [
         {
             test: /\.tsx?$/,
@@ -31,10 +31,20 @@ export const module = {
         },
     ],
 };
-export const plugins = [
+const plugins = [
     new HtmlWebpackPlugin({
         inject: true,
         template: _resolve(appDirectory, 'public/index.html'),
     }),
 ];
-export const mode = 'development';
+const mode = 'development';
+
+module.exports = {
+    entry,
+    output,
+    resolve,
+    devServer,
+    module: moduleRules,
+    plugins,
+    mode,
+};
